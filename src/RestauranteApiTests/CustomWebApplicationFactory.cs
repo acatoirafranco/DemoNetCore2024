d2e5a4b076563ae34ebfbee43c45dfe72b06004b
acatoirafranco <146419621+acatoirafranco@users.noreply.github.com>
2024-11-20 08:57:18 +0100

    First Upload

---
+++ src/RestauranteApiTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestauranteApi.Data;
+using System.Data.Common;
+
+namespace RestauranteApiTests
+{
+    public class CustomWebApplicationFactory<TProgram>
+        : WebApplicationFactory<TProgram> where TProgram : class
+    {
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<PedidoDbContext>));
+
+                services.Remove(dbContextDescriptor!);
+
+                services.AddDbContext<PedidoDbContext>(options =>
+                {
+                    var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    options.UseSqlite($"Data Source={Path.Join(path, "RestauranteApi.db")}");
+                });
+            });
+
+            builder.UseEnvironment("development");
+        }
+
+        //protected override void ConfigureWebHost(IWebHostBuilder builder)
+        //{
+        //    builder.UseStartup<TestStartup>();
+
+        //    builder.ConfigureServices(services =>
+        //    {
+        //        var dbContextDescriptor = services.SingleOrDefault(
+        //            d => d.ServiceType ==
+        //                typeof(DbContextOptions<PedidoDbContext>));
+
+        //        services.Remove(dbContextDescriptor);
+
+        //        var dbConnectionDescriptor = services.SingleOrDefault(
+        //            d => d.ServiceType ==
+        //                typeof(DbConnection));
+
+        //        services.Remove(dbConnectionDescriptor);
+
+        //        // Create open SqliteConnection so EF won't automatically close it.
+        //        services.AddSingleton<DbConnection>(container =>
+        //        {
+        //            var connection = new SqliteConnection("DataSource=:memory:");
+        //            connection.Open();
+
+        //            return connection;
+        //        });
+
+        //        services.AddDbContext<PedidoDbContext>((container, options) =>
+        //        {
+        //            var connection = container.GetRequiredService<DbConnection>();
+        //            options.UseSqlite(connection);
+        //        });
+        //    });
+
+        //    builder.UseEnvironment("Development");
+        //}
+    }
+ }
